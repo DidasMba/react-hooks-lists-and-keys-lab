eceec990dd1b9cd28aa2874782b342203fb5fdb6
DidasMba <daniel.mbarushi@gmail.com>
2023-10-28 19:40:05 +0300

    editing ProjectItem

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,3 +1,23 @@
+/*import React from "react";
+
+function ProjectItem({ name, about, technologies }) {
+  return (
+    <div className="project-item">
+      <h3>{name}</h3>
+      <p>{about}</p>
+      <div className="technologies">
+        {/* render a <span> for each technology in the technologies array */ /*}
+      </div>
+    </div>
+  );
+}
+
+export default ProjectItem;*/
+
+
+// ON TOP IT WAS AN ANCEINNE CODE , DOWN IS THE NEW JUAT I ADDED 
+
+
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
@@ -6,10 +26,13 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technology, index) => (
+          <span key={index}>{technology}</span>
+        ))}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
